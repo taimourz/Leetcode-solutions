f47484b0194657d0557a51abb95b458cc1596a8a
Taimour Afzal <taimour.rkt@gmail.com>
2025-10-06 16:47:33 +0500

    Time: 0 ms (100%), Space: 10.7 MB (46.48%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -16,3 +16,17 @@ public:
        return helper(n, nums, dp);
     }
 };
+
+/*
+- draw the recursion tree to understand the logic.
+- a lot of repeated logic, so create a dp array is needed for memoization
+- recursive function would need size of array with other required fields.
+- base case when size is 0 means we have arrive at the end of the tree. 
+- moreover, if we go out of bounds then just 0. 
+- 2 possibilities, rob current or skip. 
+- if current is robber then n-2 b/c we can't rob the very next house as its n-1 and adjacent.
+- if skip the current, then we can rob the n-1.
+- out of the above 2 options we will pick the one where robbing gives max solution.
+- also remember: account for repeated calculations
+- return
+*/
