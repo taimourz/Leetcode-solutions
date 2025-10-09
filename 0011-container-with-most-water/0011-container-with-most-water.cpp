4b9be44ad17ef1ed7bb0c686439130f5810786a9
Taimour Afzal <taimour.rkt@gmail.com>
2025-10-09 14:52:19 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0;
+        int r = height.size() - 1;
+        int max_area = INT_MIN;
+        while(l < r){
+            int breath = r - l;
+            int heightt = min(height[l], height[r]);
+            int area = breath * heightt;
+            cout<<"l "<<l<<" r "<<r<<" breath "<<breath<<" height "<<heightt << "area "<< area<<endl;
+            max_area = max(area, max_area);
+            if(height[r] < height[l]) r--;
+            else l++;
+        }
+        return max_area;
+    }
+};
+
+/*
+two pointers obviously.
+we need to move the pointer which is smaller otherwise water overflows
+also keep track of max area. 
+what is length breath. breath (smaller) length (j - i)
+
+*/
+
+
