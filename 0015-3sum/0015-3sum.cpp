ebf40a9e656273d0c9aa9382161b472ce3b06d45
Taimour Afzal <taimour.rkt@gmail.com>
2025-10-25 22:44:34 +0500

    Time: 40 ms (90.93%), Space: 29.1 MB (72.45%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,22 +4,23 @@ public:
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
         for(int i = 0; i < nums.size(); i++){
-            int right = nums.size() - 1;
-            int sum;
-            int left = i + 1;
             int current = nums[i];
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1;
+            int right = nums.size() - 1;
+
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            
             while(left < right){
-                sum = current + nums[left] + nums[right];
-                if(sum == 0){
-                    ans.push_back({current, nums[left], nums[right]});
-                    while(left < right && nums[left+1] == nums[left]) left++;
-                    while(left < right && nums[right-1] == nums[right]) right--;
+                int current_sum = current + nums[left] + nums[right];
+                if(current_sum < 0) left++;
+                else if(current_sum > 0) right--;
+                else{
+                    ans.push_back({current, nums[left], nums[right]}); 
                     left++;
-                    right--;
-                } 
-                else if(sum < 0) left++;
-                else right--;
+                    right--;                    
+                    while(left < right && nums[left-1]  == nums[left]) left++;
+                    while(left < right && nums[right+1] == nums[right]) right--;
+                }
             }
         }
         return ans;
