94da7419a6277562299c3a652071d0f46dd92eb2
Taimour Afzal <taimour.rkt@gmail.com>
2025-10-26 00:36:09 +0500

    Time: 0 ms (100%), Space: 63 MB (48.09%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        
         int l = 0;
         int r = height.size() - 1;
-        int max_area = INT_MIN;
-        while(l < r){
-            int breath = r - l;
-            int heightt = min(height[l], height[r]);
-            int area = breath * heightt;
-            max_area = max(area, max_area);
-            if(height[r] < height[l]) r--;
-            else if(height[r] > height[l]) l++;
-            else l++;
+        long long max_area = INT_MIN;
+        while(l<r){
+            if(height[l] > height[r]){
+                long long area = (r-l) * height[r];
+                max_area = max(area, max_area);                
+                r--;
+            }else{
+                long long area = (r-l) * height[l];
+                max_area = max(area, max_area);                  
+                l++;
+            }
+
         }
         return max_area;
+
+
     }
 };
-
-/*
-two pointers
-move pointer with small height b/c we want to maximize the area.
-imp thing to note x-axis starts from 0
-*/
-
-
